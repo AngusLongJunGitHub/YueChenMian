18bd7d40844555920d50124f0babd7a92ce31a76
AngusLongJunGitHub <304771572@qq.com>
2019-03-25 09:58:49 +0800

    Dome/

---
+++ Dome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dome
+//
+//  Created by 革绿信息 on 2019/3/25.
+//  Copyright © 2019年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dome/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Dome
+//
+//  Created by 革绿信息 on 2019/3/25.
+//  Copyright © 2019年 com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
